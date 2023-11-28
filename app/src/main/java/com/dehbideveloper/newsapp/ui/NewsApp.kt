dfa9798279646b8c32236fd0b16dd8e9b5a72ce4
yusufdehbi <youssefdehbi59@gmail.com>
2023-11-28 17:19:33 +0100

    adding the bottomNavigationMenu

--- app/src/main/java/com/dehbideveloper/newsapp/ui/NewsApp.kt
+++ app/src/main/java/com/dehbideveloper/newsapp/ui/NewsApp.kt
@@ -1,26 +1,47 @@
 package com.dehbideveloper.newsapp.ui
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.dehbideveloper.newsapp.BottomMenuScreen
 import com.dehbideveloper.newsapp.MockData
+import com.dehbideveloper.newsapp.ui.components.BottomMenu
+import com.dehbideveloper.newsapp.ui.screen.Categories
 import com.dehbideveloper.newsapp.ui.screen.DetailScreen
+import com.dehbideveloper.newsapp.ui.screen.Sources
 import com.dehbideveloper.newsapp.ui.screen.TopNews
 
 @Composable
 fun NewsApp(){
-    Navigation()
+    val navController = rememberNavController()
+    val scrollState = rememberScrollState()
+    MainScreen(navController = navController, scrollState = scrollState)
+
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen(navController: NavHostController, scrollState: ScrollState){
+
+    Scaffold(bottomBar = { BottomMenu(navController = navController)}) {
+        Navigation(navController, scrollState)
+    }
 }
 
 @Composable
-fun Navigation(){
-    val navController = rememberNavController()
-    val scrollState = rememberScrollState()
+fun Navigation(navController: NavHostController, scrollState: ScrollState){
     NavHost(navController = navController, startDestination = "TopNews"){
+        bottomNavigation(navController)
         composable("TopNews"){
             TopNews(navController = navController)
         }
@@ -34,3 +55,18 @@ fun Navigation(){
         }
     }
 }
+
+fun NavGraphBuilder.bottomNavigation(navController: NavController){
+    composable(BottomMenuScreen.TopNews.route){
+        TopNews(navController = navController)
+    }
+
+    composable(BottomMenuScreen.Categories.route){
+        Categories()
+    }
+
+    composable(BottomMenuScreen.Sources.route){
+        Sources()
+    }
+}
+
--- app/src/main/java/com/dehbideveloper/newsapp/ui/components/BottomMenu.kt
+++ app/src/main/java/com/dehbideveloper/newsapp/ui/components/BottomMenu.kt
@@ -1,2 +1,41 @@
 package com.dehbideveloper.newsapp.ui.components
 
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.dehbideveloper.newsapp.BottomMenuScreen
+
+@Composable
+fun BottomMenu(navController: NavController){
+    val menuItems = listOf(BottomMenuScreen.TopNews, BottomMenuScreen.Categories, BottomMenuScreen.Sources)
+
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        menuItems.forEach{
+            BottomNavigationItem(
+                label = { Text(text = it.title)},
+                alwaysShowLabel = true,
+                selected = currentRoute == it.route,
+                onClick = {
+                          navController.navigate(it.route){
+                              navController.graph.startDestinationRoute?.let {
+                                  route ->
+                                  popUpTo(route){
+                                      saveState = true
+                                  }
+                              }
+                              launchSingleTop = true
+                              restoreState = true
+                          }
+                },
+                icon = {Icon(imageVector = it.icon, contentDescription = it.title)})
+        }
+
+    }
+}
